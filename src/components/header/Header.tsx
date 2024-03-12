c63bcbdc5195346c210847c26470a56ccefa9dcf
Ohm0025 <toyohm0025@gmail.com>
2024-03-12 18:03:31 +0700

    add:hero section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,34 +4,28 @@ import shoppingBag from "../../assets/shoppingBag.svg";
 
 const Header = () => {
   return (
-    <div className="w-full min-w-[300px]">
-      <div className="flex w-[80%] h-[143px] justify-between items-center mx-auto">
-        <div className="flex justify-center items-center">
-          <img
-            src={logoWeb}
-            alt="logo-web"
-            className="min-w-[109px] h-[41px]"
-          />
-        </div>
+    <div className="flex h-[143px] justify-around items-center mx-auto min-w-[990px]">
+      <div className="flex justify-center items-center">
+        <img src={logoWeb} alt="logo-web" className="min-w-[109px] h-[41px]" />
+      </div>
 
-        <div className="font-pop text-[20px] font-medium flex gap-[60px] text-secondary">
-          <button className="hover:text-primary">Home</button>
-          <button className="hover:text-primary">Menu</button>
-          <button className="hover:text-primary">Services</button>
-          <button className="hover:text-primary">Offers</button>
-        </div>
+      <div className="font-pop text-[20px] font-medium flex gap-[60px] text-secondary mx-[30px]">
+        <button className="hover:text-primary">Home</button>
+        <button className="hover:text-primary">Menu</button>
+        <button className="hover:text-primary">Services</button>
+        <button className="hover:text-primary">Offers</button>
+      </div>
 
-        <div className="flex items-center gap-[46px] text-secondary">
-          <button className="w-[32px] h-[32px] p-[5px]">
-            <img src={magnifyGlass} alt="magnify-glass" />
-          </button>
-          <button className="w-[32px] h-[32px] p-[5px]">
-            <img src={shoppingBag} alt="shopping-bag" />
-          </button>
-          <button className="w-[179px] h-[60px] bg-primary rounded-[40px] text-[20px] text-white font-medium">
-            Register
-          </button>
-        </div>
+      <div className="flex items-center gap-[46px] text-secondary">
+        <button className="w-[40px] h-[40px] p-[5px]">
+          <img src={magnifyGlass} alt="magnify-glass" />
+        </button>
+        <button className="w-[40px] h-[40px] p-[5px]">
+          <img src={shoppingBag} alt="shopping-bag" />
+        </button>
+        <button className="w-[179px] h-[60px] bg-primary rounded-[40px] text-[20px] text-white font-medium">
+          Register
+        </button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        shadow1: "6px 27px 34px 0 rgba(0, 0, 0, 0.1)",
+        shadow2: "-2px 22px 38px 0 rgba(142,255,154,0.5)",
+      },
       colors: {
         primary: "#39DB4A",
         secondary: "#272727",
